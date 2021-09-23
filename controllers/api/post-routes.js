218cfba685cfe49834068a020bacb389a30ba47b
Hok S. Uy <hoksuy@gmail.com>
2021-09-22 20:12:40 -0700

    added some Post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,8 +2,33 @@ const router = require('express').Router();
 const Post = require('../../models/Post');
 
 router.get('/', (req, res) => {
-    Post.findAll()
+    Post.findAll({
+        attributes: [
+            'id',
+            'date',
+            'food',
+            'calories'
+        ]
+    }).then(dbPostData => {
+        res.json(dbPostData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'date', 'food', 'calories'],
+    }).then(dbPostData => {
+        res.json(dbPostData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
