79f05d1b724f037cdf26bc6b23c91666f259d71a
Hok S. Uy <hoksuy@gmail.com>
2021-09-22 20:29:48 -0700

    finished post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const { route } = require('..');
 const Post = require('../../models/Post');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     Post.findAll({
@@ -24,6 +26,59 @@ router.get('/:id', (req, res) => {
         },
         attributes: ['id', 'date', 'food', 'calories'],
     }).then(dbPostData => {
+        res.json(dbPostData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+        foods: req.body.foods,
+        calories: req.body.calories,
+        user_id: req.session.user_id
+    }).then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+router.put('/:id', withAuth, (req, res) => {
+    Post.update(
+        {
+            foods: req.body.foods,
+            calories: req.body.calories
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    ).then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
         res.json(dbPostData);
     }).catch(err => {
         console.log(err);
