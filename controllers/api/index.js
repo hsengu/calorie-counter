7ed20ef74439a63fff6f97fa5c38f878d65d2a02
George Ngo <georgengo@msn.com>
2021-09-22 19:23:21 -0700

    local changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-<<<<<<< HEAD
 
 const postRoutes = require('./post-routes.js');
 const userRoutes = require('./user-routes');
@@ -8,7 +7,6 @@ router.use('/myinfo', userRoutes);
 router.use('/tracking', postRoutes);
 
 module.exports = router; 
-=======
 const userRoutes = require('./user-routes');
 const photoRoutes = require('./photo-routes');
 
@@ -16,4 +14,3 @@ router.use('/users', userRoutes);
 router.use('/photo', photoRoutes);
 
 module.exports = router;
->>>>>>> 95c4234492ac94b1f96663eb994efaf490531ca8
