71ef411fe62bbc74bbac3c0e141d750291ab8bb2
George Ngo <georgengo@msn.com>
2021-09-22 19:42:49 -0700

    routes and model set up for user

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,14 +1,4 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-
-const postRoutes = require('./post-routes.js');
-const userRoutes = require('./user-routes');
-
-router.use('/myinfo', userRoutes);
-router.use('/tracking', postRoutes);
-
-module.exports = router; 
-=======
 const userRoutes = require('./user-routes');
 const photoRoutes = require('./photo-routes');
 
@@ -16,4 +6,3 @@ router.use('/users', userRoutes);
 router.use('/photo', photoRoutes);
 
 module.exports = router;
->>>>>>> 95c4234492ac94b1f96663eb994efaf490531ca8
---
+++ models/index.js
@@ -0,0 +1,8 @@
+const User = require('./User');
+const Photo = require('./Photo');
+const Post = require('./Post');
+
+// create associations
+
+
+module.exports = { User, Post, Vote, Comment };
