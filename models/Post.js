61a3d9dd0caadbf66d0fefcc2d13f6b80c22561c
Hok S. Uy <hoksuy@gmail.com>
2021-09-22 19:40:44 -0700

    completed Post model

---
+++ models/Post.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {};
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            default: Date.now
+        },
+        food: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        calories: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            default: 1
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+)
+
+module.exports = Post;
