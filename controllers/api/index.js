b151aa40d898d4c7784a40285640dcd68a232e2d
Hok S. Uy <hoksuy@gmail.com>
2021-09-22 19:17:43 -0700

    fixed merge conflicts

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,19 +1,10 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-
-const postRoutes = require('./post-routes.js');
-const userRoutes = require('./user-routes');
-
-router.use('/myinfo', userRoutes);
-router.use('/tracking', postRoutes);
-
-module.exports = router; 
-=======
+const postRoutes = require('./post-routes');
 const userRoutes = require('./user-routes');
 const photoRoutes = require('./photo-routes');
 
-router.use('/users', userRoutes);
+router.use('/myinfo', userRoutes);
+router.use('/tracking', postRoutes);
 router.use('/photo', photoRoutes);
 
 module.exports = router;
->>>>>>> 95c4234492ac94b1f96663eb994efaf490531ca8
