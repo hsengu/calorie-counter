16c73b00096f7f2b0bfe625c203354e870e3355e
Hok S. Uy <hoksuy@gmail.com>
2021-09-24 12:38:48 -0700

    added functionality for register

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,14 @@
 async function logout() {
-    const response = await fetch('/api/users/logout', {
+    await fetch('/api/users/logout', {
         method: 'post',
         headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+        if(response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(reponse.statusTest);
+        }
     });
-
-    if(response.ok) {
-        document.location.replace('/');
-    } else {
-        alert(response.statusText);
-    }
 }
 
 document.querySelector('#logoutBtn').addEventListener('click', logout);
