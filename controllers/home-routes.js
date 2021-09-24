a09311a4e1ceed12917d5b7514593694bb6e36f8
Hok S. Uy <hoksuy@gmail.com>
2021-09-23 19:52:10 -0700

    setup main.handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
+    res.render('home');
+});
 
+router.get('/signup', (req, res) => {
+    res.render('create');
 });
 
 router.get('/upload', (req, res) => {
